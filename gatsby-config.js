9a1497067fed9f1fb1bf328f0c98c85ca4c1b3cd
newt0 <py_lambda@yahoo.co.jp>
2020-05-30 14:43:00 +0900

    サイトメタを更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Kyoheiの履歴書',
+    author: 'newt0',
+    description: '「見やすく、更新しやすい」履歴書サイトです。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
